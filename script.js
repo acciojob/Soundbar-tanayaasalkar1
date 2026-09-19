054c70c63c51baf4149b025ed9b9903d09565ace
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-09-19 18:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let audio = new Audio(`sounds/${button.innerText}.mp3`);
+        audio.play();
+    });
+});
